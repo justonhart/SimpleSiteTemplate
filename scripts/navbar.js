963edc7c7daf946ca3b79f725fef28fca5cb5da0
Juston Hart <contact@justonh.art>
2020-11-29 10:47:05 -0600

    Add content switching to navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,1 +1,30 @@
-const homeButton = document.getElementById("home");
+const chem = document.getElementById("chemButton");
+const tele = document.getElementById("teleButton");
+const sent = document.getElementById("sentButton");
+
+//render chem content
+chem.onclick = showChemContent;
+
+//render tele content
+tele.onclick = showTeleContent;
+
+//render sentencing content
+sent.onclick = showSentenceContent;
+
+function showChemContent(){
+    document.getElementById("chemContent").classList = [];
+    document.getElementById("teleContent").classList = ["hidden"];
+    document.getElementById("sentenceContent").classList = ["hidden"];
+}
+
+function showTeleContent(){
+    document.getElementById("chemContent").classList = ["hidden"];
+    document.getElementById("teleContent").classList = [];
+    document.getElementById("sentenceContent").classList = ["hidden"];
+}
+
+function showSentenceContent(){
+    document.getElementById("chemContent").classList = ["hidden"];
+    document.getElementById("teleContent").classList = ["hidden"];
+    document.getElementById("sentenceContent").classList = [];
+}
